c048087547d1e4783d7d2ff53c82065d29833faa
Robert <nig_niggin@gmx.net>
2012-10-22 16:49:31 +0200

    Initial class. Contains main method

---
+++ Spider/src/main/Spider.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author robert
+ *
+ */
+public class Spider {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
